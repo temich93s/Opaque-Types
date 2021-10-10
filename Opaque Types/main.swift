3928a37a48ebef9d95744d9e67cd9e1db156f413
Temich93s <nil>
2021-10-10 16:19:24 +1000

    Пройдена тема: Возвращение непрозрачного типа

--- Opaque Types/main.swift
+++ Opaque Types/main.swift
@@ -80,3 +80,81 @@ print()
 // *
 // **
 // ***
+
+
+//MARK: Возвращение непрозрачного типа
+print("\n//Возвращение непрозрачного типа")
+
+struct Square: Shape {
+    var size: Int
+    func draw() -> String {
+        let line = String(repeating: "*", count: size)
+        let result = Array<String>(repeating: line, count: size)
+        return result.joined(separator: "\n")
+    }
+}
+
+let someSquare = Square(size: 3)
+print(someSquare.draw())
+print()
+
+func makeTrapezoid() -> some Shape {
+    let top = Triangle(size: 2)
+    let middle = Square(size: 2)
+    let bottom = FlippedShape(shape: top)
+    let trapezoid = JoinedShape(
+        top: top,
+        bottom: JoinedShape(top: middle, bottom: bottom)
+    )
+    return trapezoid
+}
+let trapezoid = makeTrapezoid()
+print(trapezoid.draw())
+print()
+// *
+// **
+// **
+// **
+// **
+// *
+
+func flip<T: Shape>(_ shape: T) -> some Shape {
+    return FlippedShape(shape: shape)
+}
+func join<T: Shape, U: Shape>(_ top: T, _ bottom: U) -> some Shape {
+    JoinedShape(top: top, bottom: bottom)
+}
+
+let opaqueJoinedTriangles = join(smallTriangle, flip(smallTriangle))
+print(opaqueJoinedTriangles.draw())
+print()
+// *
+// **
+// ***
+// ***
+// **
+// *
+
+//func invalidFlip<T: Shape>(_ shape: T) -> some Shape {
+//    if shape is Square {
+//        return shape // Error: return types don't match
+//    }
+//    return FlippedShape(shape: shape) // Error: return types don't match
+//}
+
+struct FlippedShape1<T: Shape>: Shape {
+    var shape: T
+    func draw() -> String {
+        if shape is Square {
+            return shape.draw()
+        }
+        let lines = shape.draw().split(separator: "\n")
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+
+func `repeat`<T: Shape>(shape: T, count: Int) -> some Collection {
+    return Array<T>(repeating: shape, count: count)
+}
+
+print(`repeat`(shape: someSquare, count: 3))
